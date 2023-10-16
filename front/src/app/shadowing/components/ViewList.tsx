b43b19ede8f3288029167dc381798871bb78f958
Daniel LEE <dmsgur7112@gmail.com>
2023-10-16 23:18:23 +0900

    refactor : 불필요한 모듈 제거, 로직 분리

---
+++ front/src/app/shadowing/components/ViewList.tsx
@@ -0,0 +1,81 @@
+'use client';
+import Link from 'next/link';
+import Image from 'next/image';
+import { useEffect } from 'react';
+import useSWRInfinite from 'swr/infinite';
+import { getShadowingList } from '@/app/api/shadowingApi';
+import useUser from '@/app/hooks/userHook';
+import { BsBookmarkPlus, BsBookmarkPlusFill } from 'react-icons/bs';
+import { listInterface } from '@/types/share';
+
+const ViewList = (props: { category: string }) => {
+  const category = props.category;
+  const { user } = useUser();
+
+  const getKey = (pageIndex: number) => {
+    const start = pageIndex * 10;
+    const end = pageIndex * 10 + 9;
+    if (category === '추천' && user.data.nickname) {
+      return `/fast/recommendations/${user.data.nickname}/${start}/${end}`;
+    } else if (category === '전체') {
+      return `/shadowings?startIndex=${start}&endIndex=${end}`; // SWR 키
+    } else {
+      return `/shadowings?startIndex=${start}&endIndex=${end}&category=${category}`; // SWR 키
+    }
+  };
+
+  const { data, size, setSize } = useSWRInfinite(getKey, getShadowingList);
+  useEffect(() => {
+    console.log(data);
+  }, [data]);
+
+  return (
+    <div className="flex flex-col my-2">
+      {data?.map((list: listInterface[]) =>
+        list.map((content: listInterface, index: number) => (
+          <Link
+            href={'/shadowing/learning/' + content.videoId}
+            key={index}
+            className="p-2 mb-3 rounded-lg w-full shadow-lg cursor-pointer bg-white flex flex-row justify-between items-center hover:bg-[#8120ff] active:bg-[#7115ea] hover:text-white hover:shadow-none box-border"
+          >
+            <div className="flex flex-row w-full items-center justify-between">
+              <div className="flex flex-row items-center mr-2 rounded-md min-w-[0px]">
+                <Image
+                  className="w-[100px] sm:w-[130px] lg:w-[160px] h-auto bg-gray-200"
+                  src="/images/default.png"
+                  width="360"
+                  height="202"
+                  blurDataURL="empty"
+                  alt="thumbnail"
+                />
+                <div className="ml-3 lg:ml-5 min-w-[0px]">
+                  <p className="text-md lg:text-xl font-medium mb-2 truncate">
+                    {content.engSentence}
+                  </p>
+                  <p className="text-xs lg:text-md truncate">
+                    {content.korSentence}
+                  </p>
+                </div>
+              </div>
+              <div className="p-3">
+                {content.isMarked ? (
+                  <BsBookmarkPlusFill color="#7D17FF" size="1.5rem" />
+                ) : (
+                  <BsBookmarkPlus color="#D9D9D9" size="1.5rem" />
+                )}
+              </div>
+            </div>
+          </Link>
+        )),
+      )}
+      <button
+        onClick={() => setSize(size + 1)}
+        className="mt-2 border border-[#e3e3e3] p-3 bg-[#f0f0f0] w-full hover:bg-white active:bg-[#f0f0f0] hover:shadow-custom rounded-xl"
+      >
+        더 보기
+      </button>
+    </div>
+  );
+};
+
+export default ViewList;
---
+++ front/src/app/shadowing/components/useCategory.tsx
@@ -0,0 +1,46 @@
+'use client';
+import { useState } from 'react';
+import { useSession } from 'next-auth/react';
+import styles from './scrollbar.module.css';
+import 'react-perfect-scrollbar/dist/css/styles.css';
+
+const useCategory = () => {
+  const { data: session } = useSession();
+  const categories = [
+    '전체',
+    '추천',
+    '북마크',
+    '음악',
+    '영화',
+    '여행',
+    '음식',
+    '일상',
+    '스포츠',
+    '비즈니스',
+    '애니메이션',
+  ];
+  if (!session) categories.splice(1, 2); // 세션이 존재하지 않는 경우 추천탭과 북마크탭 제거
+  const [selected, setSelected] = useState<number>(0);
+
+  const CatComponent = () => (
+    <ul
+      className={`flex flex-row py-2 overflow-x-auto overflow-y-hidden ${styles.scrollbar_hidden}`}
+    >
+      {categories.map((category: string, index: number) => {
+        return (
+          <li
+            key={index}
+            className={`cursor-pointer text-sm mr-2 py-1 px-5 rounded-full whitespace-nowrap shadow-md hover:text-white hover:bg-[#8120ff] active:bg-[#7115ea] ${
+              selected === index ? 'text-white bg-[#7115ea]' : 'bg-[#fff] '
+            }`}
+            onClick={() => setSelected(index)}
+          >
+            {category}
+          </li>
+        );
+      })}
+    </ul>
+  );
+  return { CatComponent, category: categories[selected] };
+};
+export default useCategory;
