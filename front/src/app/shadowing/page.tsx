dc26591f94246c2e4c8ad4b962e7c69814ff6b3a
Daniel LEE <dmsgur7112@gmail.com>
2023-10-16 23:19:18 +0900

    refactor : 카테고리, 리스트 컴포넌트화

--- front/src/app/shadowing/page.tsx
+++ front/src/app/shadowing/page.tsx
@@ -1,11 +1,15 @@
-import Categories from './components/Categories';
+'use client';
+import useCategory from './components/useCategory';
+import ViewList from './components/ViewList';
 
 const page = () => {
+  const { CatComponent, category } = useCategory();
   return (
     <div className="inner-content">
       <h1 className="text-2xl font-extrabold my-2">영어 쉐도잉</h1>
-      <p className="text-sm">영어는 씹어야 제 맛이지</p>
-      <Categories />
+      <p className="text-sm mb-4">영어는 씹어야 제 맛이지</p>
+      <CatComponent />
+      <ViewList category={category} />
     </div>
   );
 };
